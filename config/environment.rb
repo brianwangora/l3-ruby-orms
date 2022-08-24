886d2eda64e4c8316e045b44a328ec3a50a2aad1
Brian Wangora <brianwangora@gmail.com>
2022-08-24 12:53:33 +0300

    lesson complete

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,26 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+# CREATE TABLE
+Student.create_table
+
+# Create a new student
+esther = Student.new(name: "Esther", age: 18)
+chris = Student.new(name: "Chris", age: 25)
+
+# add student to database
+esther.add_to_db
+chris.add_to_db
+
+pp Student.show_all
+
+esther.name = "Water Bottle"
+esther.age =21
+esther.update_student
+
+pp Student.show_all
+
+esther.delete_student
+
+pp Student.show_all
